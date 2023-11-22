1abf2ad5a9e23396ac34a8acc0b657512086bd50
pascalism <pascalis.maschke@signavio.com>
2023-11-22 18:58:00 +0100

    add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './App.css';
 import { ThemeProvider } from '@ui5/webcomponents-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
