85eb67d78fa1ee422f3dfdf28d43a31929a0a55b
pascalism <pascalis.maschke@signavio.com>
2023-12-11 14:09:55 +0100

    filter variants on conformance

--- src/EventVariantsDisplay/index.jsx
+++ src/EventVariantsDisplay/index.jsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import {
   Timeline,
   TimelineItem,
   FlexBox,
   Card,
+  Switch,
+  Label,
 } from '@ui5/webcomponents-react';
 import { isNil, find } from 'lodash';
 
@@ -10,7 +13,7 @@ const findColor = (level) => {
   switch (level) {
     case 'faulty':
       return '#FF0000';
-    case 'partly faulty':
+    case 'partlyFaulty':
       return '#FFBF00';
     case false:
       return '#008000';
@@ -24,6 +27,8 @@ const EventVariantsDisplay = ({
   setRightClickInfo,
   setDialogIsOpen,
 }) => {
+  const [showOnlyNonConformant, setShowOnlyNonConformant] = useState(false);
+  console.log(showOnlyNonConformant);
   const chartData = newData
     ?.map(({ events, measure, isFaulty, faultyEventsFromVariant }) => {
       return {
@@ -41,48 +46,61 @@ const EventVariantsDisplay = ({
         isFaulty,
       };
     })
-    .sort((a, b) => b.measure - a.measure);
+    .sort((a, b) => b.measure - a.measure)
+    .filter(({ isFaulty }) => {
+      if (showOnlyNonConformant) {
+        return isFaulty;
+      }
+      return true;
+    });
 
   return (
-    <div style={{ overflowX: 'auto', whiteSpace: 'nowrap', height: 500 }}>
-      {chartData.map((variant, index) => (
-        <>
-          <Card
-            style={{
-              width: 150,
-              margin: '8px',
-              minHeight: 50,
-            }}
-          >
-            Occurrence: {variant.measure}
-          </Card>
-          <FlexBox
-            key={index}
-            style={{ marginBottom: '16px', height: 200, padding: 5 }}
-          >
-            <Timeline layout="Horizontal">
-              {variant.events.map(({ eventName, level }, eventIndex) => (
-                <TimelineItem
-                  nameClickable
-                  onNameClick={() => {
-                    setRightClickInfo({});
-                    setDialogIsOpen(true);
-                  }}
-                  name={eventName}
-                  key={eventIndex}
-                  style={{
-                    width: 200,
-                    margin: '8px',
-                    minHeight: 50,
-                    background: findColor(level),
-                  }}
-                />
-              ))}
-            </Timeline>
-          </FlexBox>
-        </>
-      ))}
-    </div>
+    <>
+      <Label>Only show non-conformant variants</Label>
+      <Switch
+        onChange={() => setShowOnlyNonConformant(!showOnlyNonConformant)}
+        style={{ paddingTop: 20, paddingLeft: 30 }}
+      />
+      <div style={{ overflowX: 'auto', whiteSpace: 'nowrap', height: 500 }}>
+        {chartData.map((variant, index) => (
+          <>
+            <Card
+              style={{
+                width: 150,
+                margin: '8px',
+                minHeight: 50,
+              }}
+            >
+              Occurrence: {variant.measure}
+            </Card>
+            <FlexBox
+              key={index}
+              style={{ marginBottom: '16px', height: 200, padding: 5 }}
+            >
+              <Timeline layout="Horizontal">
+                {variant.events.map((event, eventIndex) => (
+                  <TimelineItem
+                    nameClickable
+                    onNameClick={() => {
+                      setRightClickInfo(event);
+                      setDialogIsOpen(true);
+                    }}
+                    name={event.eventName}
+                    key={eventIndex}
+                    style={{
+                      width: 200,
+                      margin: '8px',
+                      minHeight: 50,
+                      background: findColor(event.level),
+                    }}
+                  />
+                ))}
+              </Timeline>
+            </FlexBox>
+          </>
+        ))}
+      </div>
+    </>
   );
 };
 
